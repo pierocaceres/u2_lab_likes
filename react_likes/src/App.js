72fac28deff49e65b4ade580a14be66d6e88538f
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-15 16:57:56 -0400

    Added +/- functionality and styling

---
+++ react_likes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Likes from './component/Likes'
+
+function App() {
+  return (
+    <div className="App">
+      <Likes />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ react_likes/src/component/Likes.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function Likes(props) {
+
+    let [likes, totalLikes] = useState(0)
+    console.log(likes)
+    const addLikes = () => {
+        likes++
+        totalLikes(likes)
+    }
+
+    const subtractLikes = () => {
+        likes--
+        totalLikes(likes)
+    }
+    return (
+        <div className="like">
+            <h2 className="likes-view">{likes}</h2>
+            <br />
+            <button className="button" id="plus" onClick={addLikes}>+</button>
+            <button className="button" id="minus" onClick={subtractLikes}>-</button>
+        </div>
+    );
+}
+
+export default Likes;
